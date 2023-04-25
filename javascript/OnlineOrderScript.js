6d88d26a493baa947b7eca5bc51c070bce01ae63
Thomas Pascual Bercasio <relempagotb@gmail.com>
2023-04-24 21:32:59 -0700

    updates to script to include alerts

--- javascript/OnlineOrderScript.js
+++ javascript/OnlineOrderScript.js
@@ -2,57 +2,49 @@
 
 //contains code to work with the menu
 
-//removes menu elements
+let cookiesOn = true;
+
+
+//this function will make an alert stating that cookies will be recorded for marketing purposes
+function orderOnlineCookieNotification()
+{
+	if(confirm("Cookies are collected to track customer data and for marketing purposes. Would you like to keep them on?")){
+		cookiesOn = true;
+	}
+	else {
+		cookiesOn = false;
+	}
+	
+	//tells user that cookies are on or off
+	if(cookiesOn){
+		alert("Cookies are enabled.");
+	}
+	else{
+		alert("Cookies are disabled.";
+	}
+		
+}
+
+//removes all menu elements
 function removeAllMenuElements()
 {
-	document.getElementById("vsAI").style.display = "none";
-	document.getElementById("vsHuman").style.display = "none";
-	document.getElementById("adjustSettings").style.display= "none";
-	document.getElementById("displayControls").style.display  = "none";
-	document.getElementById("returnToMenu").style.display = "none";
+	changeDisplay("none", "none","none","none","none","none");
 }
 
-//adds menu elements back
-function addAllMenuElements()
+//adds all menu elements back
+function restoreDisplay()
 {
-	document.getElementById("returnToMenu").style.display = "contents";
+	dchangeDisplay("block", "block" , "block" , "block", "block", "block");
 }
 
-//renders all game elements
-function renderGameElements()
-{		
-	//TODO instead of changing text upload sprite images
-	//renders game elements
-	//displays whether player is attacking or defending
-	document.getElementById("gameDisplay").style.display = "block";
-	document.getElementById("playerOne").style.display = "inline-block";
-	document.getElementById("playerTwo").style.display = "inline-block";
-	document.getElementById("p1Status").style.display = "block";
-	document.getElementById("p2Status").style.display = "block";
-	//displays how much health each player has left
-	document.getElementById("p1HealthBar").style.display = "block";
-	document.getElementById("p2HealthBar").style.display = "block";
-	//displays player models
-	document.getElementById("p1Model").style.display = "block";
-	document.getElementById("p2Model").style.display = "block";
-	//displays player movesets
-	//during AI play player2 moveset will be visible but not interactible
-	document.getElementById("p1Move").style.display = "block";
-	document.getElementById("p2Move").style.display = "block";
+// will change styling depending on arguments paassed in the parameter.
+function changeDisplay(appetizers, dimsum, noodles, beef, desserts, drinks)
+{
+	document.getElementById("appetizers").style.display = appetizers;
+	document.getElementById("dimsum").style.display = dimsum;
+	document.getElementById("noodles").style.display = noodles;
+	document.getElementById("appetizers").style.display = beef;
+	document.getElementById("appetizers").style.display = desserts;
+	document.getElementById("appetizers").style.display = drinks;
 }
-//removes game elements
-function removeGameElements()
-{		
-	document.getElementById("gameDisplay").style.display = "none";
-	document.getElementById("p1Status").style.display = "none";
-	document.getElementById("p2Status").style.display = "none";
-	document.getElementById("p1HealthBar").style.display = "none";
-	document.getElementById("p2HealthBar").style.display = "none";
-	document.getElementById("p1Model").style.display = "none";
-	document.getElementById("p2Model").style.display = "none";
-	document.getElementById("p1Move").style.display = "none";
-	document.getElementById("p2Move").style.display = "none";
-	document.getElementById("demoTimer").innerHTML = "&nbsp";
-	document.getElementById("p1Decision").innerHTML = "&nbsp";
-	document.getElementById("p2Decision").innerHTML = "&nbsp";
-}
+
