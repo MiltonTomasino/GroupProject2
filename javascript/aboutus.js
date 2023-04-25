f39208a96ade033c080d1950d4664ac21e671fa2
student <alpha.omr.diallo@gmail.com>
2023-04-25 09:20:48 -0700

    Final changes for about us page

--- javascript/aboutus.js
+++ javascript/aboutus.js
@@ -4,9 +4,24 @@ const deliveryMusic = new Audio("./audio/uber_eats_jingle.mp3");       // consta
 // Event Listener for clicking the ad
 uberEatsAd.addEventListener('click', () => {                            // upon clicking the add play delivery music
     deliveryMusic.play();
-    deliveryMusic.onended = function() {
+    deliveryMusic.onended = function() {                                // when music ends, change page to order online
     window.location.href = "./orderonline.html";
     };
 });
 
+// Image Carousel
+let slideIndex = 0;
+carousel();
+
+function carousel() {
+    let i;
+    let x = document.getElementsByClassName("aboutUsSlides");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > x.length) {slideIndex = 1}
+    x[slideIndex-1].style.display = "block";
+    setTimeout(carousel, 5000); // Change image every 5 seconds
+}
 
